135b2ca87e3d1e601f0610c019ea572cec833f39
ssgreg <me@ssgreg.com>
2014-05-08 13:05:57 +0400

    + deploy (configuring images)

---
+++ LostSeriesServer/deploy.sh
@@ -0,0 +1,11 @@
+
+# images
+
+sudo apt-get install libjpeg-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
+sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
+
+sudo pip install -I pillow
+sudo pip install -I http://effbot.org/media/downloads/Imaging-1.1.7.tar.gz
+
